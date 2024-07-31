6f8fee403871c7aab6872e9e33e70d32952d5045
freetimezp <kraftukrnet@ukr.net>
2024-07-31 17:48:51 +0300

    02. js-portfolio-107-coffee. Setup. Assets files.

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+import React from 'react';
+import Hero from '@/components/Hero';
+
+const Home = () => {
+  return (
+    <div className='h-full overflow-x-hidden'>
+      <Hero />
+    </div>
+  );
+}
+
+export default Home;
